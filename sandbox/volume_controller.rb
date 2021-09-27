ad2d438c28aedf11efa3eb7510a5ea9af2497ea8
Tom Wang <me@tom.af>
2021-09-27 09:39:59 +0800

    BufferedMonitor + hax

--- sandbox/volume_controller.rb
+++ sandbox/volume_controller.rb
@@ -4,40 +4,21 @@ DEVICE = '/dev/input/event0'
 
 class ValueBuffer
   def initialize
-    @buffer_period = 100
     @buffered_value = 0
-    @last_access_time = time_now_ms
-  end
-
-  def full?
-    with_access_update do
-      (time_now_ms - @last_access_time) > @buffer_period
-    end
   end
 
   def push(value)
-    with_access_update do
-      @buffered_value += value
-    end
+    @buffered_value += value
   end
 
   def flush
-    with_access_update do
-      value = @buffered_value
-      @buffered_value = 0
+    value = @buffered_value
+    @buffered_value = 0
 
-      return value
-    end
+    return value
   end
 
   private
-
-  def with_access_update
-    return_value = yield
-    @last_access_time = time_now_ms
-
-    return return_value
-  end
 
   def time_now_ms
     (Time.now.to_f * 1000).to_i
@@ -45,11 +26,24 @@ class ValueBuffer
 end
 
 class Adjustment
-  ADJUSTMENT_RATIO = 100
-
   def initialize(value)
     @value = value
-    @normalized_value = @value / ADJUSTMENT_RATIO
+    @normalized_value = @value
+      case
+      when @value.abs <= 10
+        0
+      when @value.abs <= 40
+        if @value > 0
+          1
+        else
+          -1
+        end
+      when @value.abs <= 40
+        @value / 10
+      else
+        @value / 40
+      end
+    puts "#{@value} => #{@normalized_value}" if @value != 0
   end
 
   def empty?
@@ -60,39 +54,37 @@ class Adjustment
     adjustment_str =
       if @normalized_value < 0
         "- #{@normalized_value.abs}"
+        "- 1"
       else
         "+ #{@normalized_value}"
+        "+ 1"
       end
 
     "ddcutil setvcp 62 #{adjustment_str}"
   end
 end
 
-class VolumeController
+class BufferedMonitor
   def initialize
+    @buffer_period = 20
     @buffer = ValueBuffer.new
-  end
 
-  def run
-    volume_handler = ->(value) do
-      if @buffer.full?
-        puts "Full!"
-        value = @buffer.flush
-        adjust_volume_by(value)
-      else
-        puts "Pushing #{value}"
-        @buffer.push(value)
+    Thread.new do
+      loop do
+        buffered_value = @buffer.flush
+        _adjust_volume_by(buffered_value)
+        sleep @buffer_period.to_f / 1000
       end
     end
-
-    Dialer.new(device: DEVICE, rotate_handler: volume_handler).run
-  end
-
-  def test
-    puts 'test'
   end
 
   def adjust_volume_by(value)
+    @buffer.push(value)
+  end
+
+  private
+
+  def _adjust_volume_by(value)
     adjustment = Adjustment.new(value)
     return if adjustment.empty?
 
@@ -101,6 +93,21 @@ class VolumeController
     puts command
     system(command)
   end
+end
+
+class VolumeController
+  def initialize
+    @monitor = BufferedMonitor.new
+  end
+
+  def run
+    volume_handler = ->(value) do
+      @monitor.adjust_volume_by(value)
+    end
+
+    Dialer.new(device: DEVICE, rotate_handler: volume_handler).run
+  end
+
 end
 
 VolumeController.new.run
